1ee3252d9c88dbb25fe98f20fa5771a8ab44cd6d
joaopscalazans <joaopscalzans@gmail.com>
2026-07-29 19:33:32 -0300

    exercio um

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Interfaces;
+﻿using Interfaces.ex01;
+
+namespace Interfaces;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Ex01();
+    }
+
+    static void Ex01()
+    {
+        Console.WriteLine("Falar ===============");
+        new List<IAnimal>
+        {
+            new Cachorro(),
+            new Gato(),
+            new Passaro()
+        }.ForEach(animal => animal.Falar());
+        Console.WriteLine("Mover ===============");
+        new List<IAnimal>
+        {
+            new Cachorro(),
+            new Gato(),
+            new Passaro()
+        }.ForEach(animal => animal.Mover());
+        Console.WriteLine();
     }
 }
