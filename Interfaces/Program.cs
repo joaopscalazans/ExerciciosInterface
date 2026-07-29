14bc1981781cbf671e5a9712d66a0868a0eba99a
joaopscalazans <joaopscalzans@gmail.com>
2026-07-29 20:22:16 -0300

    exercicio tres

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Interfaces.ex01;
 using Interfaces.ex02;
+using Interfaces.ex03;
 
 namespace Interfaces;
 
@@ -9,6 +10,7 @@ class Program
     {
         Ex01();
         Ex02();
+        Ex03();
     }
 
     static void Ex01()
@@ -47,6 +49,41 @@ class Program
             forma.CalcularPerimetro();
             forma.CalcularArea();
             Console.WriteLine("--------");
+        }
+        
+        Console.WriteLine();
+    }
+
+    static void Ex03()
+    {
+        Console.WriteLine("============EX03=======");
+        List<IReprodutorMultimidia> rms = new List<IReprodutorMultimidia>
+        {
+            new PlayerMp3(),
+            new PlayerStreaming(),
+            new PlayerVideo()
+        };
+
+        foreach (var rm in rms)
+        {
+            if (rm is PlayerStreaming streaming)
+            {
+                Console.WriteLine("PlayerStreaming");
+            }
+            else if (rm is PlayerVideo vid)
+            {
+                Console.WriteLine("PlayerVideo");
+            }else if (rm is PlayerMp3 mp3)
+            {
+                Console.WriteLine("PlayerMp3");
+            }
+            
+            rm.Play();
+            rm.Pause();
+            rm.Avancar();
+            rm.Play();
+            rm.Stop();
+            Console.WriteLine();
         }
         
         Console.WriteLine();
