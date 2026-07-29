d8ddd2f80ebde02c0cf01a17ce663927eca8cbf1
joaopscalazans <joaopscalzans@gmail.com>
2026-07-29 19:58:25 -0300

    exercicio doiz

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Interfaces.ex01;
+using Interfaces.ex02;
 
 namespace Interfaces;
 
@@ -7,10 +8,12 @@ class Program
     static void Main(string[] args)
     {
         Ex01();
+        Ex02();
     }
 
     static void Ex01()
-    {
+    { 
+        Console.WriteLine("===========EX01==========");
         Console.WriteLine("Falar ===============");
         new List<IAnimal>
         {
@@ -25,6 +28,27 @@ class Program
             new Gato(),
             new Passaro()
         }.ForEach(animal => animal.Mover());
+        Console.WriteLine();
+    }
+
+    static void Ex02()
+    {
+        Console.WriteLine("===========EX02==========");
+        List<IFormaGeometrica> formas = new List<IFormaGeometrica>
+        {
+            new Circulo(10),
+            new Retangulo(8, 5),
+            new Triangulo(3, 4, 5, 4)
+        };
+
+        foreach (var forma in formas)
+        {
+            Console.WriteLine("--------");
+            forma.CalcularPerimetro();
+            forma.CalcularArea();
+            Console.WriteLine("--------");
+        }
+        
         Console.WriteLine();
     }
 }
