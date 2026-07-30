bbc68fa9ef071d16168a408cbbef7e247abdf974
joaopscalazans <joaopscalzans@gmail.com>
2026-07-29 21:54:24 -0300

    finalizado

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,6 +6,8 @@ using Interfaces.ex04;
 using Interfaces.ex05;
 using Interfaces.ex06;
 using Interfaces.ex07;
+using Interfaces.ex08;
+using Interfaces.ex09;
 
 namespace Interfaces;
 
@@ -19,6 +21,8 @@ class Program
         Ex05();
         Ex06();
         Ex07();
+        Ex08();
+        Ex09();
     }
 
     static void Ex01()
@@ -154,7 +158,7 @@ class Program
             Console.WriteLine();
             Console.WriteLine(x.GetType().Name);
             if(x is NotificadorEmail)
-            x.EnviarMensagem("Foi autorizado no seu cartão Nubank um valor de R$2.000.\n Se foi o senhor(a) o autor do pagamento\n por favor nos encaminhar o cpf e os tres digitos na parte de trás do cartão", "Maria do Socorro");
+                x.EnviarMensagem("Foi autorizado no seu cartão Nubank um valor de R$2.000.\n Se foi o senhor(a) o autor do pagamento\n por favor nos encaminhar o cpf e os tres digitos na parte de trás do cartão", "Maria do Socorro");
             else if(x is NotificadorWhatsApp || x is NotificardorSMS)
                 x.EnviarMensagem("Foi autorizado no seu cartão Nubank um valor de R$2.000.\n Se foi o senhor(a) o autor do pagamento\n por favor nos encaminhar o cpf e os tres digitos na parte de trás do cartão", "+559999999-9999");
         });
@@ -169,6 +173,63 @@ class Program
             new Livro(),
             new Movel()
         }.ForEach(x => x.CalcularFrete(50));
+    }
+    
+    static void Ex08()
+    {
+        Console.WriteLine("============EX08=======");
+        Console.WriteLine("!! FEITO AO PE DA LETRA!!");
+        new List<IAtacantes>
+        {
+            new Dragao(),
+            new Mago(),
+            new Paladino()
+        }.ForEach(x =>
+        {
+            Console.WriteLine("Personagens que conseguem atacar");
+            Console.WriteLine(x.GetType().Name);
+        });
+    }
+    
+    static void Ex09()
+    {
+        Console.WriteLine("============EX09=======");
+        IRelatorios relatorios = null;
+        IFormasBusca formasBusca = null;
+      
+        Console.WriteLine("Onde consultar");
+            Console.WriteLine("1)  - Consultar no banco");
+            Console.WriteLine("2)  - Consultar no arquivo X");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+             case "1":
+                 formasBusca = new BancoDados();
+                 break;
+             case "2":
+                 formasBusca = new Arquivo();
+                 break;
+            }
+            
+            Console.WriteLine("Tipo de relatorio");
+            Console.WriteLine("1)  - PDF");
+            Console.WriteLine("2)  - CSV");
+            opcao = Console.ReadLine();
+            
+        
+            switch (opcao)
+            {
+                case "1":
+                    relatorios = new PDF();
+                    break;
+                case "2":
+                    relatorios = new CSV();
+                    break;
+            }
+
+            formasBusca.Buscar();
+            relatorios.GerarRelatorio();
     }
     
     
